e057a195adf988a78e77d21fe31ceb1797b5873a
Carlos Dagorret <dagorret@eco.unrc.edu.ar>
2021-07-15 22:07:31 -0300

    add auth from django

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,8 @@
-from django.urls import path
+from django.urls import path, include
 from users.views import dashboard
 
 
 urlpatterns = [
+    path('accounts/', include("django.contrib.auth.urls")),
     path('dashboard/', dashboard, name='dashboard')
 ]
